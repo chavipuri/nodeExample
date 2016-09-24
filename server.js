430e85bd131fc32063e521988745d77cda5d185b
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-09-24 13:56:15 -0700

    Docker1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var  app = express();
 app.get('/', function (req, res) {
-  res.send('Hey Chavi & Ankush\n');
+  res.send('Hey! Chavi & Ankush\n');
 });
 
 app.listen(process.env.PORT || 5000);
