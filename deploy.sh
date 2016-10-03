b3fcdde63ffffc8c3fdcdfa1b2d43172b2f8c4ed
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-03 11:23:03 -0700

    Docker Container Deployment

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 docker push chavipuri/node-web-app
 
 ssh ubuntu@54.161.223.251 <<EOF
-    docker pull chavi/node-web-app:latest
-    docker run -p 5000:5000 -d chavi/node-web-app:latest
-#EOF
+    docker pull chavipuri/node-web-app:latest
+    docker run -p 5000:5000 -d chavipuri/node-web-app:latest
+EOF
