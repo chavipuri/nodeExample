fe4b60ce9788ad442344055390040cdf29fea710
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-03 11:01:49 -0700

    Testing 3 and Deploy with DockerHub

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+docker push chavi/nodeExample
+
 ssh ubuntu@54.161.223.251 <<EOF
-    git clone https://github.com/chavipuri/nodeExample.git
-    cd ~/nodeExample
-    docker build -t chavi/node-web-app .
-    docker run -p 5000:5000 -d chavi/node-web-app
-    exit
+    docker pull chavi/nodeExample:latest
+    docker run -p 5000:5000 -d chavi/nodeExample:latest
 EOF
