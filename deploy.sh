81a7dbc45a38b36fc63823792aec9d73aeb5b51e
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-03 11:06:04 -0700

    Changed repo name for docker

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker push chavi/nodeExample
+docker push chavi/node-web-app
 
 ssh ubuntu@54.161.223.251 <<EOF
-    docker pull chavi/nodeExample:latest
-    docker run -p 5000:5000 -d chavi/nodeExample:latest
+    docker pull chavi/node-web-app:latest
+    docker run -p 5000:5000 -d chavi/node-web-app:latest
 EOF
