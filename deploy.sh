4d7b2a8c0a4ca7431929ce217e284a755252cfc9
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-03 10:29:54 -0700

    Testing 2 and Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ssh ubuntu@54.196.4.44 <<EOF
+ssh ubuntu@54.161.223.251 <<EOF
+    git clone https://github.com/chavipuri/nodeExample.git
     cd ~/nodeExample
-    git pull
     docker build -t chavi/node-web-app .
     docker run -p 5000:5000 -d chavi/node-web-app
     exit
