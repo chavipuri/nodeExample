a3c666a028bd633745c786c8771d7e18a6290f98
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-03 11:41:47 -0700

    Docker Container Deployment with tagging

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,9 @@ docker push chavipuri/node-web-app
 
 ssh ubuntu@54.161.223.251 <<EOF
     docker pull chavipuri/node-web-app:latest
-    docker run -p 5000:5000 -d chavipuri/node-web-app:latest
+    docker stop web || true
+    docker rm web || true
+    docker rmi chavipuri/node-web-app:current || true
+    docker tag chavipuri/node-web-app:latest chavipuri/node-web-app:current
+    docker run -p 5000:5000 --name web -d chavipuri/node-web-app:current
 EOF
