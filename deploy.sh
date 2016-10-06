d8b1b37ad9183e3ebf957675a71230c6bdf5d5a0
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-05 17:40:15 -0700

    Jenkins server on EC2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+#docker login -e "chavi.malhotra@gmail.com" -u "chavipuri" -p "ankush"
+#docker push chavipuri/node-web-app
 
-docker push chavipuri/node-web-app
-
-ssh ubuntu@54.161.223.251 <<EOF
-    docker pull chavipuri/node-web-app:latest
-    docker stop web || true
-    docker rm web || true
-    docker rmi chavipuri/node-web-app:current || true
-    docker tag chavipuri/node-web-app:latest chavipuri/node-web-app:current
-    docker run -p 5000:5000 --name web -d chavipuri/node-web-app:current
+ssh ubuntu@54.172.3.252 <<EOF
+#    docker pull chavipuri/node-web-app:latest
+#    docker stop web || true
+#    docker rm web || true
+#    docker rmi chavipuri/node-web-app:current || true
+#    docker tag chavipuri/node-web-app:latest chavipuri/node-web-app:current
+    docker run -p 5000:5000 --name web -d chavipuri/node-web-app
 EOF
