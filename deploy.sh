8b1f18b0371edf679ee0e33e51150d6346c2c406
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-05 18:17:35 -0700

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#docker login -e "chavi.malhotra@gmail.com" -u "chavipuri" -p "ankush"
-#docker push chavipuri/node-web-app
 
 ssh ubuntu@54.172.3.252 <<EOF
-#    docker pull chavipuri/node-web-app:latest
-#    docker stop web || true
-#    docker rm web || true
-#    docker rmi chavipuri/node-web-app:current || true
-#    docker tag chavipuri/node-web-app:latest chavipuri/node-web-app:current
-    docker run -p 5000:5000 --name web -d chavipuri/node-web-app
+   docker stop web || true
+   docker rm web || true
+   docker rmi chavi/node-web-app || true
+   mkdir nodeExample
+   cd ~/nodeExample
+   git pull
+   docker build -t chavi/node-web-app .
+   docker run -p 5000:5000 --name web -d chavi/node-web-app 
 EOF
