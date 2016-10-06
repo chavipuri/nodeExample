f298892f82e2ae384f895d3fc3a2c8140d089967
Chavi Malhotra <chavi.malhotra@gmail.com>
2016-10-05 18:34:06 -0700

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ ssh ubuntu@54.172.3.252 <<EOF
    docker stop web || true
    docker rm web || true
    docker rmi chavi/node-web-app || true
-   mkdir nodeExample
    cd ~/nodeExample
    git pull
    docker build -t chavi/node-web-app .
